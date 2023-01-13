80e70b585943463e74a27e8746c40e6091a9971a
Adin Ackerman <adinackerman@gmail.com>
2023-01-13 15:25:09 -0800

    Initial Commit

---
+++ GettingStartedWithPython/GettingStartedWithPythonApp.swift
@@ -0,0 +1,17 @@
+//
+//  GettingStartedWithPythonApp.swift
+//  GettingStartedWithPython
+//
+//  Created by Adin Ackerman on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct GettingStartedWithPythonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
